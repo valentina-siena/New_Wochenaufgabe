bc2602fedb0e7294c5e96794b91a7b9d22a86eb9
valentina-siena <zschav@bbcag.ch>
2024-04-05 09:41:46 +0200

    add Files

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+function toggleMainNav() {
+    let element = document.getElementById('main_menu');
+    element.classList.toggle('expanded')
+  }
+   
+  // Replace icons with svg's
+   feather.replace();
